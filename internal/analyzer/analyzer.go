a112ed7e0fa1f7eb517c9c5d99260a824cf80087
whiteasvoid <joaocurtooo@gmail.com>
2026-01-15 00:31:38 +0000

    Improved PrintReport so now it displays colors for better UX. File analyzer.go created but not initialized yet.

---
+++ internal/analyzer/analyzer.go
@@ -0,0 +1,5 @@
+package analyzer
+
+import (
+	_ "strings"
+)
--- internal/report/template.go
+++ internal/report/template.go
@@ -8,30 +8,48 @@ import (
 	"github.com/joaomiguelcurto/log-parser/internal/models"
 )
 
+var Reset = "\033[0m"
+var Red = "\033[31m"
+var Green = "\033[32m"
+var Yellow = "\033[33m"
+var Blue = "\033[34m"
+var Magenta = "\033[35m"
+var Cyan = "\033[36m"
+var Gray = "\033[37m"
+var White = "\033[97m"
+
 // Template format for the main report about the scan.
 const reportTemplate = `
------ Start Report -----
-Path:             {{.Path}}
-Total Lines:      {{.LineCount}}
-Lines per Second: {{printf "%s" .FormatedLinesPerSecond}}
-Duration:         {{.AnalyzeDuration}}
+{{Cyan}}----- Start Report -----{{Reset}}
+{{Red}}Path:{{Reset}}             {{.Path}}
+{{Blue}}Total Lines:{{Reset}}      {{.LineCount}}
+{{Green}}Lines per Second:{{Reset}} {{.FormatedLinesPerSecond}}
+Duration:          {{.AnalyzeDuration}}
 
------ Process Breakdown -----
+{{Yellow}}----- Process Breakdown -----{{Reset}}
 {{- range .ProcessStats}}
 Process: {{printf "%-30s" .Name}} | Count: {{.Count}}
 {{- end}}
------ End Report -----
+{{Cyan}}----- End Report -----{{Reset}}
 `
 
 // Prints the report and information about the scan.
 func PrintReport(r models.Report) {
-	// Create the template
-	tmpl, err := template.New("report").Parse(reportTemplate)
+	funcs := template.FuncMap{
+		"Red":     func() string { return Red },
+		"Green":   func() string { return Green },
+		"Yellow":  func() string { return Yellow },
+		"Blue":    func() string { return Blue },
+		"Cyan":    func() string { return Cyan },
+		"Reset":   func() string { return Reset },
+		"Magenta": func() string { return Magenta },
+	}
+
+	tmpl, err := template.New("report").Funcs(funcs).Parse(reportTemplate)
 	if err != nil {
 		panic(err)
 	}
 
-	// Execute the template
 	err = tmpl.Execute(os.Stdout, r)
 	if err != nil {
 		fmt.Printf("Error printing report: %v\n", err)
